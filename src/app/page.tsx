837aada1eb6cfcdbcd2152427952f31cc498a38f
Rameen Ihtasham <krameen500@gmail.com>
2024-10-20 19:11:11 +0500

    DB CHANGES

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import Link from "next/link";
+import { db } from "~/server/db";
 
-export default function HomePage() {
+export const dynamic = "force-dynamic";
+
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello Worldd!!!!
+      {posts.map((post) => (
+        <div key={post.id}>{post.name}</div>
+      ))}
     </main>
   );
 }
